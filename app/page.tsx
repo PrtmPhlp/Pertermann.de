3256bb3ee343c7a5517bc91482709d4fd98cbb94
PrtmPhlp <95431082+PrtmPhlp@users.noreply.github.com>
2023-12-20 21:16:25 +0100

    page.tsx aktualisieren

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ function AboutMe() {
       <p className="text-tertiary">About this</p>
       <div className="text-secondary flex flex-col gap-4">
         <p>
-          A place for everything and nothing, where expectations are left at the
+          A place for everything and nothing, where expectations are left at the ICH MAG PIZZAA
           doorstep and chaos reigns supreme.
         </p>
         <p>
