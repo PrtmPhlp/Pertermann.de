4cf8307e002b4b033493ea8b082d94761df4bf2e
PrtmPhlp <p.pertermann@icloud.com>
2023-12-25 14:25:42 +0100

    removing test message

--- app/page.tsx
+++ app/page.tsx
@@ -69,7 +69,7 @@ function AboutMe() {
           doorstep and chaos reigns supreme.
         </p>
         <p>
-          Hello World! Working on multiple projects and apps, some of them you can find here,
+          Working on multiple projects and apps, some of them you can find here,
           some you don&apos;t. Maybe also look at my{' '}
           <ExternalLink arrow={true} href="https://github.com/PrtmPhlp">
             Github
