7221f32e5efed96f97d27547541dcea272fffdb7
PrtmPhlp <p.pertermann@icloud.com>
2025-02-01 12:22:25 +0100

    🐎 ci(deploy): re-enable docker deployment workflow

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,14 +1,17 @@
 import { promises as fs } from 'node:fs';
 
 export async function GET() {
-  const file = await fs.readFile(
-    process.cwd() + '/app/api/hello/message.txt',
-    'utf8',
-  );
-  console.log(`Log: ${file}`);
-  const result = 5 * 3;
+  // const file = await fs.readFile(
+  //   process.cwd() + '/app/api/hello/message.txt',
+  //   'utf8',
+  // );
+  // console.log(`Log: ${file}`);
+  // const result = 5 * 3;
   // return new Response('Hello, Next.js!');
+  // return new Response(
+  //   `Hello, Next.js! \n5 * 3 equals: ${result} \nmessage.txt: ${file}`,
+  // );
   return new Response(
-    `Hello, Next.js! \n5 * 3 equals: ${result} \nmessage.txt: ${file}`,
+    `Hello World`,
   );
 }
